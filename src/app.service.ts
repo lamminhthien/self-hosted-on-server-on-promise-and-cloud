ccb7c4d6aa550a517af9ada9284bd337d7adc7d0
Thiện Lâm <lamminhthien02012000@gmail.com>
2024-02-11 17:51:31 +0700

    update docker swarm guide

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World from kubernetes';
+    return 'Hello World from kubernetes333';
   }
 }
